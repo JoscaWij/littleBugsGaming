6c50d58da38346f37ce6f8483e8721eed28d02fe
JoscaWij <Josca.W@gmx.net>
2020-10-24 21:50:00 +0200

    Add to story

--- client/src/stories/Card.stories.js
+++ client/src/stories/Card.stories.js
@@ -1,11 +1,12 @@
 import React from "react";
 import Card from "./../components/Card";
+import cardImageSrc from "./assets/Platzhalter.jpg";
 
 export default {
   title: "Site/Card",
   component: Card,
 };
 
-const Template = () => <Card />;
-
-export const GameCard = Template.bind({});
+export const ExampleCard = () => (
+  <Card imageSrc={cardImageSrc} gameName="GAME" isReady={false} />
+);
