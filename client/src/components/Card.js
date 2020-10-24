e3ed04bbbc398f463645f3815d1ff7bb1912759d
JoscaWij <Josca.W@gmx.net>
2020-10-24 18:09:35 +0200

    Add picture and styling

--- client/src/components/Card.js
+++ client/src/components/Card.js
@@ -1,7 +1,22 @@
 import React from "react";
+import styled from "@emotion/styled";
+import PlatzhalterImageSrc from "./../assets/Platzhalter.jpg";
 
 const Card = () => {
-  return <div>hello</div>;
+  return <GameCard>hello</GameCard>;
 };
 
 export default Card;
+
+const GameCard = styled.div`
+  background-image: url(${PlatzhalterImageSrc});
+  background-size: cover;
+  background-repeat: no-repeat;
+  background-color: black;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border-radius: 25px;
+  width: 20vh;
+  height: 20vh;
+`;
