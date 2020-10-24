4efc4883baf949ec5316b4291ef793ad6dc0ba80
JoscaWij <Josca.W@gmx.net>
2020-10-24 17:10:45 +0200

    Update server.js

--- server.js
+++ server.js
@@ -5,6 +5,15 @@ const port = process.env.PORT || 3000;
 
 app.use(express.static(path.join(__dirname, "client/build")));
 
+app.use(
+  "/storybook",
+  express.static(path.join(__dirname, "client/storybook-static"))
+);
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client/build", "index.html"));
+});
+
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
 });
