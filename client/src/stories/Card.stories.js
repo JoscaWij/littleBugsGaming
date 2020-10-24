11dd32e680c5c06f6b8a88915b3d5c3d2a363c3d
JoscaWij <Josca.W@gmx.net>
2020-10-24 17:49:15 +0200

    Add to storybook

---
+++ client/src/stories/Card.stories.js
@@ -0,0 +1,11 @@
+import React from "react";
+import Card from "./../components/Card";
+
+export default {
+  title: "Site/Card",
+  component: Card,
+};
+
+const Template = () => <Card />;
+
+export const GameCard = Template.bind({});
