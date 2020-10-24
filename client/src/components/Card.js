0916b7479820ffc69c64cd4f855af888cd4eb964
JoscaWij <Josca.W@gmx.net>
2020-10-24 18:22:35 +0200

    Outsorce Image into own styled component

--- client/src/components/Card.js
+++ client/src/components/Card.js
@@ -3,20 +3,32 @@ import styled from "@emotion/styled";
 import PlatzhalterImageSrc from "./../assets/Platzhalter.jpg";
 
 const Card = () => {
-  return <GameCard>hello</GameCard>;
+  return (
+    <GameCard>
+      <GameCardImage /> hello
+    </GameCard>
+  );
 };
 
 export default Card;
 
 const GameCard = styled.div`
-  background-image: url(${PlatzhalterImageSrc});
-  background-size: cover;
-  background-repeat: no-repeat;
-  background-color: black;
+  background-color: rgba(0, 0, 0, 0.4);
   display: flex;
   justify-content: center;
   align-items: center;
   border-radius: 25px;
   width: 20vh;
   height: 20vh;
+`;
+
+const GameCardImage = styled.img`
+  background-image: url(${PlatzhalterImageSrc});
+  background-size: cover;
+  background-repeat: no-repeat;
+  width: inherit;
+  height: inherit;
+  z-index: -2;
+  position: absolute;
+  border-radius: inherit;
 `;
