bc71af91442a406f6bc8631b5e343d0a3a5619a8
JoscaWij <Josca.W@gmx.net>
2020-10-25 15:44:54 +0100

    Prepare header and story

---
+++ client/src/stories/Header.stories.js
@@ -0,0 +1,11 @@
+import React from "react";
+import Header from "../components/Header";
+
+export default {
+  title: "Site/Header",
+  component: Header,
+};
+
+const Template = () => <Header />;
+
+export const MainHeader = Template.bind({});
