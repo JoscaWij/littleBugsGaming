14a994b9f6cfa2ada33e441f43f7be9eb78989d9
JoscaWij <Josca.W@gmx.net>
2020-10-25 16:17:17 +0100

    Add title

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -2,7 +2,11 @@ import React from "react";
 import styled from "@emotion/styled";
 
 const Header = () => {
-  return <HeaderWrapper>Welcome</HeaderWrapper>;
+  return (
+    <HeaderWrapper>
+      <Title>Welcome</Title>
+    </HeaderWrapper>
+  );
 };
 
 export default Header;
@@ -10,4 +14,9 @@ export default Header;
 const HeaderWrapper = styled.div`
   background-color: grey;
   width: 100%;
+  text-align: center;
+`;
+
+const Title = styled.h2`
+  padding: 1em;
 `;
