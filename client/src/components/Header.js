46a791dd57648b9bb0149bcd5f1c7c7a9424bafa
JoscaWij <Josca.W@gmx.net>
2020-10-25 15:47:10 +0100

    Add styled component div

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,7 +1,13 @@
 import React from "react";
+import styled from "@emotion/styled";
 
 const Header = () => {
-  return <div></div>;
+  return <HeaderWrapper>Welcome</HeaderWrapper>;
 };
 
 export default Header;
+
+const HeaderWrapper = styled.div`
+  background-color: grey;
+  width: 100%;
+`;
