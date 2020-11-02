f522834ed6186b733beb68b7cfbd21918edc0c7f
JoscaWij <Josca.W@gmx.net>
2020-11-02 15:45:17 +0100

    Prepare MainPage and story

---
+++ client/src/stories/MainPage.stories.js
@@ -0,0 +1,11 @@
+import React from "react";
+import MainPage from "../pages/MainPage";
+
+export default {
+  title: "Site/MainPage",
+  component: MainPage,
+};
+
+const Template = () => <MainPage />;
+
+export const OverviewPage = Template.bind({});
