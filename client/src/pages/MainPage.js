ac45a0e7609ffc6f345cc84f68528ccbc3c5e83e
JoscaWij <Josca.W@gmx.net>
2020-11-02 15:49:23 +0100

    Add crads in card container

--- client/src/pages/MainPage.js
+++ client/src/pages/MainPage.js
@@ -1,7 +1,21 @@
 import React from "react";
+import styled from "@emotion/styled";
+import Card from "../components/Card";
 
 const MainPage = () => {
-  return <div>hallo</div>;
+  return (
+    <CardContainer>
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+    </CardContainer>
+  );
 };
 
 export default MainPage;
+
+const CardContainer = styled.section`
+  display: flex;
+  flex-wrap: wrap;
+`;
